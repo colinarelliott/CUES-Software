2b77625614cf6864ed2118cab3ba26b9e52920f0
Colin Elliott <colinarelliott@gmail.com>
2020-03-19 12:20:14 -0400

    Added sidebar - WIP

--- components/Layout.js
+++ components/Layout.js
@@ -1,5 +1,6 @@
 import Head from 'next/head';
 import Nav from './Nav';
+import Sidebar from './Sidebar';
 
 const Layout = (props) => (
     <div>
@@ -7,10 +8,39 @@ const Layout = (props) => (
             <title>CUES Software v1.0</title>
             <link rel="stylesheet" href="https://bootswatch.com/4/simplex/bootstrap.min.css"></link>
         </Head>
-        <Nav />
-        <div className="container">
-            {props.children}
+        <body>
+        <div className="container-fluid">
+            <Nav />
+            <div className="row">
+                <div className="col-9">
+                    {props.children}
+                </div>
+                <div className="col-3">
+                    <Sidebar/>
+                </div>
+            </div>
         </div>
+        </body>
+
+        <style jsx>{`
+            .container-fluid {
+                margin: 0px;
+                padding: 0px;
+                height: 100%;
+            }
+        `}</style>
+
+        <style global jsx>{`
+            html,
+            body,
+            body > div:first-child,
+            div#__next,
+            div#__next > div,
+            div#__next > div > div {
+                height: 100%;
+            }
+        `}</style>
+
     </div>
 );
 
---
+++ components/Sidebar.js
@@ -0,0 +1,17 @@
+const Sidebar = () => (
+    <div>
+        <h1>Test 1... 2...</h1>
+
+        <style jsx>{`
+            div {
+                height: 100%;
+                background-color: red;
+            }
+        `}</style>
+
+    </div>
+
+    
+);
+
+export default Sidebar;
