2bd748759a1f7d5dba2f57e0ad68bed3f962a3b0
Colin Elliott <colinarelliott@gmail.com>
2020-03-20 14:22:55 -0400

    Removed bootstrap, added material-ui.

--- components/Layout.js
+++ components/Layout.js
@@ -1,56 +1,33 @@
 import Head from 'next/head';
 import Nav from './Nav';
 import Sidebar from './Sidebar';
+//import AppBar from '@material-ui/core/AppBar'
 
 const Layout = (props) => (
     <div>
         <Head>
             <title>CUES Software v1.0</title>
-            <link rel="stylesheet" href="https://bootswatch.com/4/simplex/bootstrap.min.css"></link>
+            <link href="https://fonts.googleapis.com/css?family=Roboto&display=swap" rel="stylesheet"></link>
+            <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
         </Head>
-        <div className="container-fluid">
-            <div className="row" id="nav">
-                <div className="col-12">
+        <div>
+            <div>
+                <div>
                     <Nav />
+                    
                 </div>
             </div>
-            <div className="row" id="content">
-                <div className="col-9">
+            <div>
+                <div>
                     {props.children}
                 </div>
-                <div className="col-3">
+                <div>
                     <Sidebar name="Properties">
                         <p>This is where properties will go</p>
                     </Sidebar>
                 </div>
             </div>
         </div>
-
-        <style jsx>{`
-            .container-fluid {
-                margin: 0px;
-                padding: 0px;
-                height: 100%;
-            }
-
-            #nav {
-                height: 7vh;
-                padding: 0px;
-                margin: 0px;
-            }
-
-            #content {
-                height: 93vh;
-                padding: 0px;
-                margin: 0px;
-            }
-
-            .col-3, .col-9, .col-12 {
-                padding: 0px;
-                margin: 0px;
-            }
-
-        `}</style>
 
         <style global jsx>{`
             html,
@@ -62,6 +39,10 @@ const Layout = (props) => (
                 height: 100%;
                 padding: 0px;
                 margin: 0px;
+            }
+
+            * {
+                font-family: 'Roboto', sans-serif;
             }
         `}</style>
 
--- components/Nav.js
+++ components/Nav.js
@@ -2,31 +2,20 @@ import Link from 'next/link'
 
 const Nav = () => (
     <div>
-    <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
-        <Link href="/"><a className="navbar-brand"><b>CUES</b></a></Link>
-        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarNav">
-            <ul className="navbar-nav">
-                <li className="nav-item">
-                    <Link href="/inv"><a className="nav-link">Inventory</a></Link>
+            <ul>
+                <li>
+                    <Link href="/inv"><a>Inventory</a></Link>
                 </li>
-                <li className="nav-item">
-                    <Link href="/cal"><a className="nav-link">Calendar</a></Link>
+                <li>
+                    <Link href="/cal"><a>Calendar</a></Link>
                 </li>
-                <li className="nav-item">
-                    <Link href="/ppl"><a className="nav-link">People</a></Link>
+                <li>
+                    <Link href="/ppl"><a>People</a></Link>
                 </li>
-                <li className="nav-item">
-                    <Link href="/evt"><a className="nav-link">Events</a></Link>
+                <li>
+                    <Link href="/evt"><a>Events</a></Link>
                 </li>
             </ul>
-        </div>
-    </nav>
-    <style jsx>{`
-        
-    `}</style>
     </div>
 );
 
